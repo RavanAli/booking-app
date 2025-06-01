c1065915526d66beabc1530d2365febff0243311
j <eliyevrevan60@gmail.com>
2025-06-01 17:25:51 +0400

    Dto

--- src/main/java/org/example/bookingapp/model/dto/BookingDto.java
+++ src/main/java/org/example/bookingapp/model/dto/BookingDto.java
@@ -1,4 +1,20 @@
 package org.example.bookingapp.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Component
 public class BookingDto {
+    private Long id;
+    private Long flightId;
+    private long passengerId;
+    private String passengerName;
+    private Integer numberOfSeats;
 }
--- src/main/java/org/example/bookingapp/model/dto/PassengerDto.java
+++ src/main/java/org/example/bookingapp/model/dto/PassengerDto.java
@@ -1,4 +1,21 @@
 package org.example.bookingapp.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Component
 public class PassengerDto {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String passport;
+
 }
